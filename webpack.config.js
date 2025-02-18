0e0fb4dbabb200dba15e16b2848c3f3a7ae652d7
FedeWaelk <fedewaelk@gmail.com>
2025-02-18 11:32:57 -0300

    webpack initial configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Limpia la carpeta 'dist' antes de cada compilación
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Plantilla HTML personalizada
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true, // Redirige todas las rutas al index.html
+  },
+};
